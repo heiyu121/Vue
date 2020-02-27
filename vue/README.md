28b7616b4f70591da6f0a01ae267772cf3f100c7
heiyu121 <1572006569@qq.com>
2020-02-27 11:19:11 +0800

    change

--- vue/README.md
+++ vue/README.md
@@ -19,3 +19,36 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## git
+
+```py
+# 查看本地分支
+$ git branch
+* master
+
+# 查看远程分支
+git branch -r
+
+# 查看所有分支
+git branch -a
+
+# 本地创建新的分支
+git branch [branch name]
+
+# 切换到新的分支
+git checkout [branch name]
+
+# 创建+切换分支
+git checkout -b [branch name]
+
+# 将新分支推送到github
+git push origin [branch name]
+
+# 删除本地分支
+git branch -d [branch name]
+
+# 查看自己所在分支
+git status
+```
+
---
+++ vue/src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl" />
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      // 有错误，轮播的点出不来
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://img1.qunarzz.com/qs/1912/96/2c110e7b64063502.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://img1.qunarzz.com/qs/2001/d3/2859c13f92a45202.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper>>> .swiper-pagination-bullet-active
+    background: #fff;
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom : 31.25%
+    background : #eee
+    .swiper-img
+      width : 100%
+</style>
